da83e3b18b324c311167c6a536a5d165dbfadbeb
vrishadrockstar <vrishadrockstar@gmail.com>
2021-08-17 22:07:22 +0530

    accept issue

--- api/data/data.controller.js
+++ api/data/data.controller.js
@@ -33,8 +33,6 @@ module.exports = {
             "Accept-Encoding": "gzip, deflate, br",
             Accept:
               acceptHeader,
-			"User-Agent":
-			  "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.64 Safari/537.11",
             cookie: cookie,
           },
         });
