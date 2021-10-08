98d445c2b70a2e5d25091bbc1b7641217669b051
vrishadrockstar <87621066+vrishadrockstar@users.noreply.github.com>
2021-10-08 12:43:15 +0530

    Update data.controller.js

--- api/data/data.controller.js
+++ api/data/data.controller.js
@@ -6,7 +6,7 @@ const arrTypes = ["NIFTY", "BANKNIFTY", "FINNIFTY"];
 const acceptHeader =
   "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8";
 const cookie =
-  "D27F1340BB381D9C07E3B17AC8D8E87E~Q+89kGGcacT2IbGIIMW1CCSHKW5nrdy3PJJaG4jQs0bjI6p6RkWKsvIOeJmhJCGr0H9pqUTyhqar1HovdD6LfcNcllxQhmS4p+Kc663tVEMOi0rgOy8CGYl251dTb2QjZe24NWss2qboA0OLD3Wig4vI6oyuvomGyWEVtT0i2No=; Domain=.nseindia.com; Path=/; Max-Age=7183; HttpOnly";
+  "bm_sv=D27F1340BB381D9C07E3B17AC8D8E87E~Q+89kGGcacT2IbGIIMW1CCSHKW5nrdy3PJJaG4jQs0bjI6p6RkWKsvIOeJmhJCGr0H9pqUTyhqar1HovdD6LfcNcllxQhmS4p+Kc663tVEP+wOckOi6/Lfs1gAM7ZyB59/IoNfLYgtV+O3A3s+IUGXE6/0D8kl19eVSoFgmZ8yE=; Domain=.nseindia.com; Path=/; Max-Age=6028; HttpOnly";
 
 module.exports = {
   getData: (req, res, next) => {
